93a95928f98c2571903ab68c6533491a59af2f30
Piyanshu Mangla <piyanshumangla456@gmail.com>
2022-08-06 22:37:13 +0530

    doing some changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -335,28 +335,3 @@ function urlJoiner(fav, state){
     
 }
 
-// // ToDo: Create an array of starter topics
-
-// // ToDo: Get input to add a new topic button
-
-// // ToDo: set up ajax
-// // ToDo: Create a variable for the api url and search topic
-
-// Functions 
-// // ToDo: make something to generate and append the buttons
-
-// // ToDo: print 10 gif thumbnails to the page
-
-// // ToDo: Show gif PG rating (better make it look nice)
-
-// // ToDo: make gif start/stop running on click;
-// // ToDO: make gif start on hover 
-
-
-/**
- * ! Bonus
- * * Add a persistent favorites section (possibly with cookies)
- * * Make this responsive
- * * Integrate with other APIs
- * * make it so you can scroll down and get 10 more gifs
- */
